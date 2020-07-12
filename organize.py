a46c6843b6d41340ec130f4fbbbead280ad12216
pjbrunner <pjbrunner542@gmail.com>
2020-07-11 23:03:31 -0600

    Create separate income and expense dataframes

--- organize.py
+++ organize.py
@@ -5,34 +5,50 @@ import pandas as pd
 
 def organize_files(*args):
     # Read and concatenate files into single dataframe.
-    df = pd.concat(map(pd.read_csv, *args))
+    raw_df = pd.concat(map(pd.read_csv, *args))
+    trimmed_df = pd.DataFrame(raw_df, columns = ['Posting Date', 'Amount'])
     if debug:
         # Can't use a starred expression in fstrings so I used format this time.
         print('Files given: {}'.format(*args))
-        print(f'Combined dataframe:\n{df}')
-    organize_expenses(df)
+        print(f'Raw dataframe:\n{raw_df}\n')
+        print(f'Trimmed dataframe:\n{trimmed_df}\n')
+    income = organize_income(trimmed_df)
+    expenses = organize_expenses(trimmed_df)
 
-def organize_expenses(expense_dataframe):
-    df = pd.DataFrame(expense_dataframe, columns = ['Posting Date', 'Amount'])
+def organize_income(df):
+    income = df.copy()
     # Rename columns.
-    df.columns = ['Date', 'Expense']
+    income = income.drop(income[income.Amount < 0].index)
+    income.columns = ['Date', 'Income']
     # Convert dates to format that pandas can work with.
-    df['Date'] = pd.to_datetime(df['Date'])
+    income['Date'] = pd.to_datetime(income['Date'])
     # Make the Date column the new index.
-    df.index = df['Date']
-    del df['Date']
-    df = df.sort_index()
+    income.index = income['Date']
+    del income['Date']
+    income = income.sort_index()
+
+    if debug:
+        print(f'Organized income:\n{income}\n')
+    return income
+
+def organize_expenses(expenses):
+    # Rename columns.
+    expenses.columns = ['Date', 'Expense']
+    # Convert dates to format that pandas can work with.
+    expenses['Date'] = pd.to_datetime(expenses['Date'])
+    # Make the Date column the new index.
+    expenses.index = expenses['Date']
+    del expenses['Date']
+    expenses = expenses.sort_index()
 
     # Remove all rows with positive numbers.
-    df = df.drop(df[df.Expense > 0].index)
+    expenses = expenses.drop(expenses[expenses.Expense > 0].index)
+    # TODO: Determine if needed.
     # Convert all negatives prices to positive.
-    df['Expense'] = df['Expense'].abs()
+    # expenses['Expense'] = expenses['Expense'].abs()
     if debug:
-        print(f'Organized expenses:\n{df}')
-
-def organize_income(income_dataframe):
-    # TODO: Implement complementary income parser.
-    pass
+        print(f'Organized expenses:\n{expenses}\n')
+    return expenses
 
 def main():
     parser = argparse.ArgumentParser(description='Organize raw finances.')
