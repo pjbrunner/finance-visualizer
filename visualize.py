86df0077d4f5fa00c9b642f84f18f593db2cb443
pjbrunner <pjbrunner542@gmail.com>
2020-07-23 22:01:04 -0600

    Create basic pie chart method

--- visualize.py
+++ visualize.py
@@ -1,21 +1,41 @@
 import argparse
+import sys
 
+import pandas as pd
+import pygal
+
+
+def pie_chart_date_range(df, title):
+    pie_chart = pygal.Pie()
+    pie_chart.title = title
+    # Sum entire column.
+    print(df.iloc[:, 1].sum())
+    # Sum all expense entries belonging to the "Childcare" category.
+    print(df.loc[df['Category'] == 'Childcare']['Expense'].sum())
+    pie_chart.add('Childcare', abs(df.loc[df['Category'] == 'Childcare']['Expense'].sum()))
+    pie_chart.add('Car payment', abs(df.loc[df['Category'] == 'Car payment']['Expense'].sum()))
+    pie_chart.render_to_file('pie_chart.svg')
 
 def main():
     parser = argparse.ArgumentParser(description='Visualize organzied ' \
                                      'finances.')
     parser.add_argument('-d', '--debug', action='store_true', help='enable ' \
                         'debug output')
-    parser.add_argument('-i', '--income_file', default='income.csv',
-                        help='path where income CSV file will be written to, ' \
-                        ' default is "income.csv"')
-    parser.add_argument('-x', '--expenses_file', default='expenses.csv',
-                        help='path where expenses CSV file will be written ' \
-                        'to, default is "expenses.csv"')
+    parser.add_argument('income', help='CSV containing income')
+    parser.add_argument('expenses', help='CSV containing expenses')
 
     args = parser.parse_args()
     global DEBUG
     DEBUG = args.debug
+
+    try:
+        income_df = pd.read_csv(args.income)
+        expenses_df = pd.read_csv(args.expenses)
+    except FileNotFoundError as e:
+        print(e)
+        sys.exit(1)
+
+    pie_chart_date_range(expenses_df, 'Test title')
 
 if __name__ == "__main__":
     main()
