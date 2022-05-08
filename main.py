5c7ac436442bc848ea20e20e79a3550fd7891b11
pjbrunner <pjbrunner542@gmail.com>
2022-05-07 21:16:47 -0600

    Write dataframes to file using paths from JSON config file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from new_organize import categorize_data, create_data_frame, get_json_from_file, separate_expenses_and_income
+from organize import categorize_data, create_data_frame, get_json_from_file, separate_expenses_and_income, write_final_df_to_file
 
 
 def get_args():
@@ -13,13 +13,19 @@ def main():
     args = get_args()
     configs = get_json_from_file(args.file)
     dataframes = []
-    for account in configs:
-        dataframes.append(create_data_frame(configs[account]))
+
+    for account in configs['finance_data']:
+        dataframes.append(create_data_frame(configs['finance_data'][account]))
     expenses, income = separate_expenses_and_income(dataframes)
     print(f'{expenses}\n{income}')
-    detailed_income = categorize_data(income, 'income')
-    detailed_expenses = categorize_data(expenses, 'expenses')
-    print(detailed_income)
+
+    organized_income = categorize_data(income, 'income')
+    organized_expenses = categorize_data(expenses, 'expenses')
+
+    if not organized_income.empty:
+        write_final_df_to_file(organized_income, path=configs['out_files']['income_file'])
+    if not organized_expenses.empty:
+        write_final_df_to_file(organized_expenses, path=configs['out_files']['expenses_file'])
 
 if __name__ == '__main__':
     main()
